a23dbf93238e63cdb9690dc46d96b124b4b434aa
Tony Sanjuan <tonysanj13@gmail.com>
2022-07-22 23:32:57 +0200

    test: test failing

--- react-fibonacci/src/hooks/fibonacciResolver/useFibonacciResolver.unit.test.tsx
+++ react-fibonacci/src/hooks/fibonacciResolver/useFibonacciResolver.unit.test.tsx
@@ -15,40 +15,40 @@ describe('<useFibonacciResolver />', () => {
         expect(Array.isArray(sut)).toBe(true)     
     });
 
-    // it('resolve(0) output should be [0]', () => {
-    //     const { result } = renderHook(() => useFibonacciResolver());
+    it('resolve(0) output should be [0]', () => {
+        const { result } = renderHook(() => useFibonacciResolver());
     
-    //     const sut = result.current.resolve(0)
-    //     const expectedResult: number[] = [0]
+        const sut = result.current.resolve(0)
+        const expectedResult: number[] = [0]
 
-    //     expect(sut).toEqual(expectedResult)    
-    // });
+        expect(sut).toEqual(expectedResult)    
+    });
 
-    // it('resolve(1) output should be [0, 1]', () => {
-    //     const { result } = renderHook(() => useFibonacciResolver());
+    it('resolve(1) output should be [0, 1]', () => {
+        const { result } = renderHook(() => useFibonacciResolver());
     
-    //     const sut = result.current.resolve(0)
-    //     const expectedResult: number[] = [0, 1]
+        const sut = result.current.resolve(1)
+        const expectedResult: number[] = [0, 1]
 
-    //     expect(sut).toEqual(expectedResult)    
-    // });
+        expect(sut).toEqual(expectedResult)    
+    });
 
 
-    // it('resolve(5) output should be [0, 1, 1, 2, 3, 5]', () => {
-    //     const { result } = renderHook(() => useFibonacciResolver());
+    it('resolve(5) output should be [0, 1, 1, 2, 3, 5]', () => {
+        const { result } = renderHook(() => useFibonacciResolver());
     
-    //     const sut = result.current.resolve(10)
-    //     const expectedResult: number[] = [0, 1, 1, 2, 3, 5]
+        const sut = result.current.resolve(5)
+        const expectedResult: number[] = [0, 1, 1, 2, 3, 5]
 
-    //     expect(sut).toEqual(expectedResult)
-    // });
+        expect(sut).toEqual(expectedResult)
+    });
 
-    // it('resolve(10) output should be [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]', () => {
-    //     const { result } = renderHook(() => useFibonacciResolver());
+    it('resolve(10) output should be [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]', () => {
+        const { result } = renderHook(() => useFibonacciResolver());
     
-    //     const sut = result.current.resolve(10)
-    //     const expectedResult: number[] = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        const sut = result.current.resolve(10)
+        const expectedResult: number[] = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-    //     expect(sut).toEqual(expectedResult)
-    // });
+        expect(sut).toEqual(expectedResult)
+    });
 });
