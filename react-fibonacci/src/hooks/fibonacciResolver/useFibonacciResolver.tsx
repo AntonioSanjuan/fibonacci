11bf1de2f41627284d3ea362726b1addd9bb77fe
Tony Sanjuan <tonysanj13@gmail.com>
2022-07-22 23:33:25 +0200

    feat: integrated iterative engine :)

--- react-fibonacci/src/hooks/fibonacciResolver/useFibonacciResolver.tsx
+++ react-fibonacci/src/hooks/fibonacciResolver/useFibonacciResolver.tsx
@@ -1,8 +1,19 @@
-const iterativeResolve = (fibonacciIndex: number): number[] => {
-    return [0, 1];
+const INIT_STATE = [0, 1]
+
+const iterativeResolve = (requestedIndex: number): number[] => {
+    if(requestedIndex <= 0) return [INIT_STATE[0]]
+    if(requestedIndex === 1) return INIT_STATE
+
+    let output = [...INIT_STATE];
+
+    for(let currentIndex = 1; currentIndex < requestedIndex; currentIndex++) {
+        output.push(output[currentIndex] + output[currentIndex - 1])
+    }
+
+    return output;
 }
 const recursiveResolve = (fibonacciIndex: number): number[] => {
-    return [0, 1];
+    return INIT_STATE;
 }
 
 export const useFibonacciResolver = () => {
